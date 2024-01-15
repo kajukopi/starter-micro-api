1d6f68531ed45db7ac64654932b8449bd29b1eee
Kaju Kopi <kajukopi@gmail.com>
2024-01-15 10:06:02 +0700

    a

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const cors = require("cors");
 
 // Google Spread Sheets
 const { GoogleSpreadsheet } = require("google-spreadsheet");
