a65c92e6a0e0ea1e5be05909ce524f4405ef8d49
Kaju Kopi <kajukopi@gmail.com>
2024-01-22 15:36:02 +0700

    z

--- router/status.js
+++ router/status.js
@@ -10,25 +10,14 @@ router.get("/", async (req, res) => {
     const sheet = doc.sheetsByTitle["users"];
     const rows = await sheet.getRows();
     const find = gsToFind(rows, cookies.email);
-    console.log(rows[find].get("last_login"));
-
-    // The target date
     const targetDate = new Date(rows[find].get("last_login"));
-
-    // The current date and time
     const currentDate = new Date();
-
-    // Calculate the time difference in milliseconds
     const timeDifference = currentDate.getTime() - targetDate.getTime();
-
-    // Convert the time difference to hours
     const hoursDifference = timeDifference / (1000 * 60 * 60);
-
     console.log("Hours difference:", hoursDifference);
-
     res.json({ status: true });
   } catch (error) {
-    res.json({ status: false });
+    res.json({ status: false, error });
   }
 });
 
