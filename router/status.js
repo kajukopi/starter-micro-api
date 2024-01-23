ab0f700a9370632206b7e3f1f5012c7878b0b5de
Kaju Kopi <kajukopi@gmail.com>
2024-01-23 17:19:33 +0700

    1

--- router/status.js
+++ router/status.js
@@ -16,6 +16,22 @@ router.get("/", async (req, res) => {
   }
 });
 
+router.get("/logout", async (req, res) => {
+  try {
+    const [token] = req.rawHeaders.filter((item) => item.startsWith("Bearer"));
+    await doc.loadInfo();
+    const sheet = doc.sheetsByTitle["users"];
+    const rows = await sheet.getRows();
+    const find = gsToFind(rows, token.split(" ")[1]);
+    if (find === null) throw "Silahkan login terlebih dahulu!";
+    rows[find].assign({ token: "" });
+    await rows[find].save();
+    res.json({ status: true, data: { email: rows[find].get("email"), name: rows[find].get("name") } });
+  } catch (error) {
+    res.json({ status: false, error });
+  }
+});
+
 function gsToFind(rows, token) {
   let find = null;
   for (let i = 0; i < rows.length; i++) {
