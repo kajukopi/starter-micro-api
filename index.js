2eccb5abb7ee858819a1449e0c6b525cee18fd01
Kaju Kopi <kajukopi@gmail.com>
2024-06-03 11:20:26 +0700

    update comit

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const cookieParser = require("cookie-parser");
 
 const app = express();
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 
 app.use(express.json());
 
